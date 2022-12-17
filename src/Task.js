f0f735cf494d8a9ecbb87787d006a742182e11d0
iktoning <iktoningpuspa21@gmail.com>
2022-12-17 22:05:52 +0800

    commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://leafy-salamander-65712b.netlify.app" // TODO: replace this
+const studentName = "I Ketut Oning Pusparama" // TODO: replace this
+const studentId = "FE2862397" // TODO: replace this
+
+export { url, studentName, studentId }
